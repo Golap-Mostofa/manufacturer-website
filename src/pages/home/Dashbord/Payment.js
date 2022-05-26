4d426e6c54b6b8edf85cf54f9f8a65e925a1a214
Golap-mostofa <mostofa6410@gmail.com>
2022-05-26 16:43:54 +0600

    payment-conocetoen

--- src/pages/home/Dashbord/Payment.js
+++ src/pages/home/Dashbord/Payment.js
@@ -1,9 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Lod from '../../sheard/Lod';
+import CheckoutForm from './CheckoutForm';
 
 const Payment = () => {
     const [user] = useAuthState(auth);
@@ -14,29 +17,29 @@ const Payment = () => {
     if (isLoading) {
         return <Lod></Lod>
     }
-    console.log(user);
+    const stripePromise = loadStripe('pk_test_51L0f50IawdrGN2QNNfpnsVtZxtypMcOduvIOjMZGVHtw3JrS8VvjmlScnPOs6hqQDWeXdcVYNUxG4cboXDCom4tS00Ame6Z3CS');
     return (
         <div>
 
-
-           
-                    <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                        <div class="card-body">
-                            <h2>Hello,<span className='text-indigo-500'>{user?.displayName}</span></h2>
-                            <h2 className='text-xl font-semibold text-green-600'>{booked.name}</h2>
-                            <p><small className='text-success'>{booked.discription}</small></p>
-                            <p className='text-red-500'>${booked.price}</p>
-                        </div>
-                    </div>
-                    <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                        <div class="card-body">
-
-
-
-                        </div>
-                    </div>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <h2>Hello,<span className='text-indigo-500'>{user?.displayName}</span></h2>
+                    <h2 className='text-xl font-semibold text-green-600'>{booked.name}</h2>
+                    <p><small className='text-success'>{booked.discription}</small></p>
+                    <p className='text-red-500'>${booked.price}</p>
                 </div>
-          
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+
+                </div>
+            </div>
+        </div>
+
     );
 };
 
