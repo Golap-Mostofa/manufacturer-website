64650bd6bc06e985b68dd2b4f9c3a7394c3afdbf
Golap-mostofa <mostofa6410@gmail.com>
2022-05-26 15:44:51 +0600

    payment

--- src/pages/home/Dashbord/Payment.js
+++ src/pages/home/Dashbord/Payment.js
@@ -5,16 +5,36 @@ import Lod from '../../sheard/Lod';
 
 const Payment = () => {
     const { id } = useParams()
-    const url = `https://gentle-oasis-52101.herokuapp.com/booking/${id}`;
+    const url = `http://localhost:5000/product/${id}`;
+
     const { data: booked, isLoading } = useQuery(['booking', id], () => fetch(url).then(res => res.json()))
+    if (isLoading) {
+        return <Lod></Lod>
+    }
     console.log(booked);
-    // if (isLoading) {
-    //     return <Lod></Lod>
-    // }
     return (
         <div>
-            <h2 className='text-2xl font-semibold text-green-600'>please pay for:{id}</h2>
-            
+
+
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div class="card w-50 max-w-md bg-base-100 shadow-xl">
+                        <div class="card-body">
+                            <h2 className='text-2xl font-semibold text-green-600'>{booked.name}</h2>
+                            <p>{booked.discription}</p>
+                            <p className='text-red-500'>{booked.price}</p>
+                        </div>
+                    </div>
+                    <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                        <div class="card-body">
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
